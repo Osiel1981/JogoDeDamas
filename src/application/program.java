8ec03cf782ace39da83a0ea7894defc6525f6bc1
Osiel1981 <osielfotografia@gmail.com>
2022-11-07 19:09:50 -0300

    Alterei o arquivo Program.java e criei as classes Board Posicion e Piece

--- src/application/program.java
+++ src/application/program.java
@@ -1,7 +1,10 @@
 package application;
 
+import boardgame.Position;
+
 public class program {
 	public static void main(String[] args) {
-		System.out.print("Olá mundo");
+		Position pos = new Position(5, 3);
+		System.out.println(pos);
 	}
 }
